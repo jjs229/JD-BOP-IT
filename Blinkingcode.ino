eb77b106ed9b86d3da6eccef72769a53ab38d051
jjs229 <78002601+jjs229@users.noreply.github.com>
2022-10-10 13:26:06 -0400

    Initial "Hello World" Files
    A starting point for team members to branch from

---
+++ Blinkingcode.ino
@@ -0,0 +1,20 @@
+//Setting up i/o
+void setup() {
+  pinMode(6, OUTPUT);
+  pinMode(9, INPUT);
+}
+
+//Main  loop that has blinking light function
+void loop() {
+  if(digitalRead(9)==HIGH)
+  {
+    digitalWrite(6,HIGH);
+    delay(500);
+    digitalWrite(6,LOW);
+    delay(500);
+  }
+  else
+  {
+    digitalWrite(6,LOW);
+  }
+}
---
+++ three_blinking_lights.ino
@@ -0,0 +1,31 @@
+void setup() {
+  //Setting up I/O
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, INPUT);
+}
+void loop() {
+  //This is the loop that goes across the three LED's
+  if(digitalRead(9)==HIGH)
+  {
+    digitalWrite(6,HIGH);
+    delay(500);
+    digitalWrite(6,LOW);
+    delay(500);
+    digitalWrite(7,HIGH);
+    delay(500);
+    digitalWrite(7,LOW);
+    delay(500);
+    digitalWrite(8,HIGH);
+    delay(500);
+    digitalWrite(8,LOW);
+    delay(500);
+  }
+  else
+  {
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
+  }
+}
